0619113c7c14ac5d253dbdf314a785ad93ae9485
alokKumarSingh89 <itsaloksingh2011@gmail.com>
2026-09-11 11:21:14 +0530

    feat: update prisma configuration and add new dependencies

--- apps/project-subgraph/prisma.config.ts
+++ apps/project-subgraph/prisma.config.ts
@@ -5,10 +5,11 @@ dotenv.config({
   path: '../../.env',
 });
 export default defineConfig({
-  schema: './prisma/schema.prisma',
+  schema: 'prisma/schema.prisma',
 
   migrations: {
-    path: './prisma/migrations',
+    path: 'prisma/migrations',
+    seed: 'tsx prisma/seed.ts',
   },
 
   datasource: {
