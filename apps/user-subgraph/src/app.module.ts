020c4771db7da921e1a1c71fd71bfd8fa7ed192d
alokKumarSingh89 <itsaloksingh2011@gmail.com>
2026-09-11 17:33:52 +0530

    feat: add graphql foundation to user subgraph

--- apps/user-subgraph/src/app.module.ts
+++ apps/user-subgraph/src/app.module.ts
@@ -4,14 +4,32 @@ import { MembershipModule } from './memberships/membership.module';
 import { OrganizationModule } from './organizations/organization.module';
 import { TeamModule } from './teams/team.module';
 import { UserModule } from './users/user.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ConfigModule } from '@nestjs/config';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { UserResolver } from './users/graphql/user.resolver';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      sortSchema: true,
+      graphiql: true,
+      context: ({ req }) => ({
+        req,
+      }),
+    }),
     DatabaseModule,
     UserModule,
     OrganizationModule,
     MembershipModule,
     TeamModule,
   ],
+  providers: [UserResolver],
 })
 export class AppModule {}
